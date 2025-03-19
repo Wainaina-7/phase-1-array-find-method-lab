55b3c14eeff8fb6fc931d4b7e343a037d76c70a2
Peter kanyori <peterynaenah23@gmail.com>
2025-03-19 13:45:39 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+    function superbowlWin(arr) {
+        // Find the first object with result "W" and return the year of that object
+        const win = arr.find(item => item.result === "W");
+        
+        // Return the year of the win, or undefined if no win is found
+        return win ? win.year : undefined;
+      }
+    
